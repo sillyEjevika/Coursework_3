6bd4bfa8f9f7353eb8f96be9f3f73d17e7f43c3c
sillyEjevika <Doroteia_Vika@mail.ru>
2023-03-14 10:45:01 +0300

    курсовая 3

---
+++ Coursework_3-master/src/test/java/sillyEjevika/socksstore/SocksStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package sillyEjevika.socksstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
